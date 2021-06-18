35ba42d405c263db3b7c1ae08404e4a72c4a3f56
Mario Franze <mfranze@codingden.de>
2021-06-18 20:36:46 +0200

    Changed player pos to allready updated method

--- src/server/internalEvents/playerConnect.ts
+++ src/server/internalEvents/playerConnect.ts
@@ -1,7 +1,7 @@
 import * as alt from "alt-server";
 
 alt.on("playerConnect", (player: alt.Player) => {
-  player.pos = { x: -630.07, y: -236.332, z: 38.05704 };
+  player.pos = new alt.Vector3(-630.07, -236.332, 38.05704);
 
   // Change to a random dimension between 100 - 200
   player.dimension = Math.floor(Math.random() * Math.floor(100)) + 100;
